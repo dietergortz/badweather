7869b1734bd96068e9f170f800ff1f033dc5ce9e
dietergortz <d.g@tuta.com>
2024-08-12 18:18:35 +0100

    Making tunnel work for both predicted and current weather

--- current_weather.py
+++ current_weather.py
@@ -1,12 +1,22 @@
 from bs4 import BeautifulSoup as bs
-from datetime import datetime as dt, timedelta
+from datetime import datetime as dt
+import pandas as pd
 import requests
 import re
+import db_connect # Hides my passwords
+import pytz
+import sys
 
-current_date = dt.now().date()
-current_time = dt.now().time().hour
+utc_now = dt.now(pytz.utc)
+london_tz = pytz.timezone('Europe/London')
+current_date = utc_now.astimezone(london_tz)
+current_hour = current_date.time().hour
 year = current_date.year
-weather_data = []
+
+# Since PythonAnywhere's tasks cannot be configured to specific time zones
+# I need to run the task every hour and check if the converted hour is correct.
+if current_hour not in (12, 13, 15, 18, 00):
+    sys.exit()
 
 url = 'https://www.yr.no/en/forecast/daily-table/2-2643743/United%20Kingdom/England/Greater%20London/London'
 head = {
@@ -18,13 +28,14 @@ head = {
 }
 
 response = requests.get(url, headers=head)
+current_weather = []
 
 if response.status_code == 200:
     soup = bs(response.text, 'html.parser')
-    current_weather = soup.select('div[class^="now-hero__next-hour-content"]')
+    weather_data = soup.select('div[class^="now-hero__next-hour-content"]')
 
-    if current_weather:
-        for item in current_weather:
+    if weather_data:
+        for item in weather_data:
             type = item.find('img', class_='weather-symbol__img')
             if type:
                 type = type.get('alt')
@@ -39,8 +50,17 @@ if response.status_code == 200:
             if rain:
                 rain = rain.text
 
-            weather_data.append([current_date, False, 0, current_time, type, int(temp), int(rain)])
+            current_weather.append([current_date, 0, 0, current_hour, type, int(temp), int(rain)])
 
             break
 
-print(weather_data)
+if len(current_weather) > 0:
+    tunnel = db_connect.open_tunnel()
+    tunnel.start()
+    engine = db_connect.start_engine(tunnel)
+
+    headers = ['date', 'prediction', 'day_offset', 'time', 'type', 'temp', 'rain']
+    current_weather = pd.DataFrame(current_weather, columns=headers)
+    current_weather.to_sql(name='weather_data', con=engine, if_exists='replace', index=False)
+
+    tunnel.stop()
--- predicted_weather.py
+++ predicted_weather.py
@@ -1,17 +1,27 @@
 from bs4 import BeautifulSoup as bs
 from datetime import datetime as dt, timedelta
+import pandas as pd
 import requests
 import re
+import db_connect # Hides my passwords
+import pytz
+import sys
 
-current_date = dt.now().date()
-current_time = dt.now().time().hour
-days_to_capture = 2
-year = current_date.year
+utc_now = dt.now(pytz.utc)
+london_tz = pytz.timezone('Europe/London')
+current_date = utc_now.astimezone(london_tz)
+current_hour = current_date.time().hour
+days_to_capture = 4
+
+# Since PythonAnywhere's tasks cannot be configured to specific time zones
+# I need to run the task every hour and check if the converted hour is correct.
+if current_hour not in (12, 13, 15, 18, 00):
+    sys.exit()
 
 url = 'https://www.bbc.co.uk/weather/2643743'
 head = {
     'Accept': '*/*',
-    'Accept-Encoding': 'gzip, deflate, br, zstd',
+    'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'en-GB,en;q=0.5',
     'Connection': 'keep-alive',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0'
@@ -24,10 +34,10 @@ temperature_class = 'wr-value--temperature--c'
 precipitation_class = 'wr-time-slot-primary__precipitation'
 rain_text_class = 'wr-u-font-weight-500'
 
-weather_data = []
+predicted_weather = []
 
 for i in range(days_to_capture):
-    date = current_date + timedelta(days=i)
+    date = current_date.date() + timedelta(days=i)
     response = requests.get(url + '/day' + str(i), headers=head)
     
     if i == 0:
@@ -64,6 +74,15 @@ for i in range(days_to_capture):
                             if chance:
                                 chance = chance.group()
 
-                    weather_data.append([date, True, i, int(hour.text), type.text.lower(), int(temp), int(chance)])
+                    predicted_weather.append([date, 1, i, int(hour.text), type.text.lower(), int(temp), int(chance)])
 
-print(weather_data)
+if len(predicted_weather) > 0:
+    tunnel = db_connect.open_tunnel()
+    tunnel.start()
+    engine = db_connect.start_engine(tunnel)
+
+    headers = ['date', 'prediction', 'day_offset', 'time', 'type', 'temp', 'rain']
+    predicted_weather = pd.DataFrame(predicted_weather, columns=headers)
+    predicted_weather.to_sql(name='weather_data', con=engine, if_exists='replace', index=False)
+
+    tunnel.stop()
